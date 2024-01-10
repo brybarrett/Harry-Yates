e4e3034d66e157eb32bf4d2f176ebd52d061e2ff
Harr-Yates <hyates1@gmail.com>
2024-01-10 15:25:04 +0100

    added coordinates

--- cities.js
+++ cities.js
@@ -1,4 +1,350 @@
 const citiesByCountry = {
+  // A
+  Australia: {
+    Sydney: {
+      lat: "-33.8688",
+      lon: "151.2093",
+      name: "Sydney",
+    },
+  },
+  Austria: {
+    Vienna: {
+      lat: "48.2082",
+      lon: "16.3738",
+      name: "Vienna",
+    },
+  },
+  // B
+  Bahamas: {
+    Nassau: {
+      lat: "25.0343",
+      lon: "-77.3963",
+      name: "Nassau",
+    },
+  },
+  Barbados: {
+    Bridgetown: {
+      lat: "13.0975",
+      lon: "-59.6145",
+      name: "Bridgetown",
+    },
+  },
+  Brazil: {
+    Brasilia: {
+      lat: "-15.8267",
+      lon: "-47.9218",
+      name: "Brasília",
+    },
+  },
+  // C
+  China: {
+    Beijing: {
+      lat: "39.9042",
+      lon: "116.4074",
+      name: "Beijing",
+    },
+  },
+  Costa_Rica: {
+    SanJose: {
+      lat: "9.9281",
+      lon: "-84.0907",
+      name: "San José",
+    },
+  },
+  Croatia: {
+    Zagreb: {
+      lat: "45.8150",
+      lon: "15.9819",
+      name: "Zagreb",
+    },
+  },
+  Cuba: {
+    Havana: {
+      lat: "23.1136",
+      lon: "-82.3666",
+      name: "Havana",
+    },
+  },
+  // D
+  Denmark: {
+    Copenhagen: {
+      lat: "55.6761",
+      lon: "12.5683",
+      name: "Copenhagen",
+    },
+  },
+  Dominican_Republic: {
+    SantoDomingo: {
+      lat: "18.4861",
+      lon: "-69.9312",
+      name: "Santo Domingo",
+    },
+  },
+  // E
+  Egypt: {
+    Cairo: {
+      lat: "30.0444",
+      lon: "31.2357",
+      name: "Cairo",
+    },
+  },
+  // F
+  Fiji: {
+    Suva: {
+      lat: "-18.1248",
+      lon: "178.4501",
+      name: "Suva",
+    },
+  },
+  France: {
+    Paris: {
+      lat: "48.8566",
+      lon: "2.3522",
+      name: "Paris",
+    },
+  },
+  // G
+  Germany: {
+    Berlin: {
+      lat: "52.5200",
+      lon: "13.4050",
+      name: "Berlin",
+    },
+  },
+  Greece: {
+    Athens: {
+      lat: "37.9838",
+      lon: "23.7275",
+      name: "Athens",
+    },
+  },
+  Grenada: {
+    StGeorges: {
+      lat: "12.0564",
+      lon: "-61.7485",
+      name: "St. George's",
+    },
+  },
+  Guatemala: {
+    GuatemalaCity: {
+      lat: "14.6349",
+      lon: "-90.5069",
+      name: "Guatemala City",
+    },
+  },
+  // H
+  Honduras: {
+    Tegucigalpa: {
+      lat: "14.0723",
+      lon: "-87.1921",
+      name: "Tegucigalpa",
+    },
+  },
+  // I
+  India: {
+    NewDelhi: {
+      lat: "28.6139",
+      lon: "77.2090",
+      name: "New Delhi",
+    },
+  },
+  Indonesia: {
+    Jakarta: {
+      lat: "-6.2088",
+      lon: "106.8456",
+      name: "Jakarta",
+    },
+  },
+  Italy: {
+    Rome: {
+      lat: "41.9028",
+      lon: "12.4964",
+      name: "Rome",
+    },
+    Milan: {
+      lat: "45.4642",
+      lon: "9.1900",
+      name: "Milan",
+    },
+  },
+  // J
+  Jamaica: {
+    Kingston: {
+      lat: "17.9712",
+      lon: "-76.7929",
+      name: "Kingston",
+    },
+  },
+  Japan: {
+    Tokyo: {
+      lat: "35.6895",
+      lon: "139.6917",
+      name: "Tokyo",
+    },
+  },
+  Jordan: {
+    Amman: {
+      lat: "31.9454",
+      lon: "35.9284",
+      name: "Amman",
+    },
+  },
+  // K
+  // L
+  // M
+  Maldives: {
+    Male: {
+      lat: "4.1755",
+      lon: "73.5093",
+      name: "Malé",
+    },
+  },
+  Mauritius: {
+    PortLouis: {
+      lat: "-20.1605",
+      lon: "57.5012",
+      name: "Port Louis",
+    },
+  },
+  Mexico: {
+    MexicoCity: {
+      lat: "19.4326",
+      lon: "-99.1332",
+      name: "Mexico City",
+    },
+  },
+  Monaco: {
+    Monaco: {
+      lat: "43.7374",
+      lon: "7.4208",
+      name: "Monaco",
+    },
+  },
+  // N
+  Namibia: {
+    Windhoek: {
+      lat: "-22.5597",
+      lon: "17.0832",
+      name: "Windhoek",
+    },
+  },
+  Nepal: {
+    Kathmandu: {
+      lat: "27.7172",
+      lon: "85.3240",
+      name: "Kathmandu",
+    },
+  },
+  Netherlands: {
+    Amsterdam: {
+      lat: "52.3676",
+      lon: "4.9041",
+      name: "Amsterdam",
+    },
+  },
+  // O
+  // P
+  Panama: {
+    PanamaCity: {
+      lat: "8.9824",
+      lon: "-79.5199",
+      name: "Panama City",
+    },
+  },
+  Philippines: {
+    Manila: {
+      lat: "14.5995",
+      lon: "120.9842",
+      name: "Manila",
+    },
+  },
+  Portugal: {
+    Lisbon: {
+      lat: "38.7223",
+      lon: "-9.1393",
+      name: "Lisbon",
+    },
+  },
+  // Q
+  Qatar: {
+    Doha: {
+      lat: "25.2854",
+      lon: "51.5310",
+      name: "Doha",
+    },
+  },
+  // R
+  // S
+  Saint_Kitts_and_Nevis: {
+    Basseterre: {
+      lat: "17.3026",
+      lon: "-62.7177",
+      name: "Basseterre",
+    },
+  },
+  Saint_Lucia: {
+    Castries: {
+      lat: "14.0101",
+      lon: "-60.9874",
+      name: "Castries",
+    },
+  },
+  Saint_Vincent_and_the_Grenadines: {
+    Kingstown: {
+      lat: "13.1618",
+      lon: "-61.2248",
+      name: "Kingstown",
+    },
+  },
+  SaudiArabia: {
+    Riyadh: {
+      lat: "24.7136",
+      lon: "46.6753",
+      name: "Riyadh",
+    },
+  },
+  Seychelles: {
+    Victoria: {
+      lat: "-4.6191",
+      lon: "55.4513",
+      name: "Victoria",
+    },
+  },
+  Singapore: {
+    Singapore: {
+      lat: "1.3521",
+      lon: "103.8198",
+      name: "Singapore",
+    },
+  },
+  South_Africa: {
+    Pretoria: {
+      lat: "-25.7461",
+      lon: "28.1881",
+      name: "Pretoria",
+    },
+  },
+  South_Korea: {
+    Seoul: {
+      lat: "37.5665",
+      lon: "126.9780",
+      name: "Seoul",
+    },
+  },
+  Spain: {
+    Madrid: {
+      lat: "40.4168",
+      lon: "-3.7038",
+      name: "Madrid",
+    },
+  },
+  Sri_Lanka: {
+    Colombo: {
+      lat: "6.9271",
+      lon: "79.8612",
+      name: "Colombo",
+    },
+  },
   Sweden: {
     Stockholm: {
       lat: "59.3293",
@@ -6,21 +352,87 @@ const citiesByCountry = {
       name: "Stockholm",
     },
   },
-  UnitedKingdom: {
+  Switzerland: {
+    Bern: {
+      lat: "46.9480",
+      lon: "7.4474",
+      name: "Bern",
+    },
+  },
+  // T
+  Tanzania: {
+    Dodoma: {
+      lat: "-6.1629",
+      lon: "35.7516",
+      name: "Dodoma",
+    },
+  },
+  Thailand: {
+    Bangkok: {
+      lat: "13.7563",
+      lon: "100.5018",
+      name: "Bangkok",
+    },
+  },
+  Trinidad_and_Tobago: {
+    Port_of_Spain: {
+      lat: "10.6549",
+      lon: "-61.5019",
+      name: "Port of Spain",
+    },
+  },
+  Turkey: {
+    Ankara: {
+      lat: "39.9334",
+      lon: "32.8597",
+      name: "Ankara",
+    },
+  },
+  // U
+  United_Arab_Emirates: {
+    Abu_Dhabi: {
+      lat: "24.4539",
+      lon: "54.3773",
+      name: "Abu Dhabi",
+    },
+  },
+  United_Kingdom: {
     London: {
       lat: "51.5074",
       lon: "-0.1278",
       name: "London",
     },
-  },
-  Italy: {
-    Milan: {
-      lat: "45.4642",
-      lon: "9.1900",
-      name: "Milan",
+    Oxford: {
+      lat: "51.7520",
+      lon: "-1.2577",
+      name: "Oxford",
     },
   },
-  // Add more countries and cities as needed
+  United_States_of_America: {
+    Washington_DC: {
+      lat: "38.8951",
+      lon: "-77.0369",
+      name: "Washington, D.C.",
+    },
+  },
+  // V
+  Vietnam: {
+    Hanoi: {
+      lat: "21.0278",
+      lon: "105.8342",
+      name: "Hanoi",
+    },
+    Ho_Chi_Minh_City: {
+      lat: "10.8231",
+      lon: "106.6297",
+      name: "Ho Chi Minh City",
+    },
+  },
+
+  // W
+  // X
+  // Y
+  // Z
 };
 
 module.exports = citiesByCountry;
